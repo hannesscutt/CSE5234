4b469a2c29a08850e45eb90bea5cd9b546d60826
Johannes Scutt <johannesscutt@gmail.com>
2023-09-18 14:51:09 -0400

    Lab 2

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,10 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {createRoot} from 'react-dom/client';
+import { StrictMode } from 'react';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+//import ReactDOM from 'react-dom';
 
-var element = React.createElement('h1', { className: 'greeting' }, 'Hello, world!');
-ReactDOM.render(element, document.getElementById('root'));
+//var element = React.createElement('h1', { className: 'greeting' }, 'Hello, world!');
+//const container = document.getElementById('App');
+//const root = createRoot(container);
+//root.render(element);
+//ReactDOM.render(element, document.getElementById('root'));
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
 
 reportWebVitals();
